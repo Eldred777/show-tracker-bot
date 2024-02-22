c7c98d86cbdc244658a70f0fcac35b85becb9d7e
Ciaran KG <55831661+Eldred777@users.noreply.github.com>
2024-02-22 18:45:08 +1000

    working echo bot

--- src/show_tracker_bot/core.clj
+++ src/show_tracker_bot/core.clj
@@ -2,60 +2,56 @@
 (ns show-tracker-bot.core
   (:require
    [clojure.edn :as edn]
-   [clojure.core.async :refer [chan close!]]
-   [discljord.messaging :as discord-rest]
-   [discljord.connections :as discord-ws]
-   [discljord.formatting :refer [mention-user]]
-   [discljord.events :refer [message-pump!]]))
+   [clojure.core.async :as async]
+   [discljord.messaging :as discord-mess]
+   [discljord.connections :as discord-conn]
+   [discljord.formatting :as discord-form]
+   [discljord.events :as discord-events]))
 
 (def state (atom nil))
-
 (def bot-id (atom nil))
-
 (def config (edn/read-string (slurp "config.edn")))
-(def token (edn/read-string (slurp "token.edn")))
+(def token (edn/read-string (slurp "token")))
+(def intents    #{:guilds :guild-messages})
 
 (defmulti handle-event
-  "Event handling multi method. Dispatches on the type of the event."
-  (fn [type _data] type))
+  (fn [event-type _]
+    event-type))
 
-(defn random-response [user]
-  (str (rand-nth (:responses config)) ", " (mention-user user) \!))
+(defmethod handle-event :default 
+  [_ _])
 
 (defmethod handle-event :message-create
-  [_ {:keys [channel-id author mentions] :as _data}]
-  (when (some #{@bot-id} (map :id mentions))
-    (discord-rest/create-message! (:rest @state) channel-id :content (random-response author))))
+  [_ {{bot :bot} :author :keys [channel-id content]}]
+  (if (= content "!disconnect")
+    (do
+      (discord-mess/create-message! (:rest @state) channel-id :content "Goodbye!")
+      (discord-conn/disconnect-bot! (:gateway @state)))
+    (when-not bot
+      (discord-mess/create-message! (:rest @state) channel-id :content content))))
 
 (defmethod handle-event :ready
   [_ _]
-  (discord-ws/status-update! (:gateway @state) :activity (discord-ws/create-activity :name (:playing config))))
+  (discord-conn/status-update! (:gateway @state) 
+                               :activity (discord-conn/create-activity :name "anime 😎" :type :watch)))
 
-(defmethod handle-event :default [_ _])
-
-(defn start-bot!
-  "Start a discord bot using the token specified in `config.edn`.
-
-  Returns a map containing the event channel (`:events`), the gateway connection (`:gateway`) and the rest connection (`:rest`)."
-  [token & intents]
-  (let [event-channel (chan 100)
-        gateway-connection (discord-ws/connect-bot! token event-channel :intents (set intents))
-        rest-connection (discord-rest/start-connection! token)]
+(defn start-bot! [token]
+  (let [event-channel (async/chan 100)
+        gateway-connection (discord-conn/connect-bot! token event-channel :intents (set intents))
+        rest-connection (discord-mess/start-connection! token)]
     {:events  event-channel
      :gateway gateway-connection
      :rest    rest-connection}))
 
-(defn stop-bot!
-  "Takes a state map as returned by [[start-bot!]] and stops all the connections and closes the event channel."
-  [{:keys [rest gateway events] :as _state}]
-  (discord-rest/stop-connection! rest)
-  (discord-ws/disconnect-bot! gateway)
-  (close! events))
+(defn stop-bot! [{:keys [rest gateway events] :as _state}]
+  (discord-mess/stop-connection! rest)
+  (discord-conn/disconnect-bot! gateway)
+  (async/close! events))
 
-(defn -main [& args]
-  (reset! state (start-bot! (:token token)))
-  (reset! bot-id (:id @(discord-rest/get-current-user! (:rest @state))))
-  (try
-    (message-pump! (:events @state) handle-event)
-    (finally (stop-bot! @state))))
-
+(defn -main []
+  (println "starting bot")
+  (reset! state (start-bot! token))
+  ;; (reset! bot-id (:id @(discord-mess/get-current-user! (:rest @state))))
+  (try (discord-events/message-pump! (:events @state) handle-event)
+       (finally
+         (stop-bot! @state))))
---
+++ src/show_tracker_bot/database.clj
@@ -0,0 +1,7 @@
+(ns show-tracker-bot.database)
+
+(def database (atom nil))
+
+(defn dump-database [] database)
+
+(defn save-database [] 0)
